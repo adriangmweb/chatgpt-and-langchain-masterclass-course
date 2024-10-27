6190024207953adccca01204bc9f46694871d9da
adriangmweb <adriangonzalezmesa@gmail.com>
2024-10-27 17:22:10 +0000

    add new chain and combine it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -17,14 +17,36 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"],
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt
+test_prompt = PromptTemplate(
+    template="Write a unit test for the following {language} code:\n{code}",
+    input_variables=["language", "code"]
 )
 
-result = code_chain({
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = chain({
     "language": args.language,
     "task": args.task
 })
 
-print(result["text"])
+print(">>>> GENERATED CODE:")
+print(result["code"])
+
+print(">>>> GENERATED TEST:")
+print(result["test"])
