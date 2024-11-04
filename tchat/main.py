819ab3c9093535a185939a49d2703926946eafaf
adriangmweb <adriangonzalezmesa@gmail.com>
2024-11-04 12:41:08 +0000

    add summarize

--- tchat/main.py
+++ tchat/main.py
@@ -1,16 +1,17 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import FileChatMessageHistory, ConversationSummaryBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(
+memory = ConversationSummaryBufferMemory(
     chat_memory = FileChatMessageHistory("chat-memory.json"),
     memory_key="messages",
+    llm=chat,
     return_messages=True
 )
 
@@ -25,7 +26,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=chat,
     prompt=prompt,
-    memory=memory
+    memory=memory,
+    verbose=True
 )
 
 while True:
