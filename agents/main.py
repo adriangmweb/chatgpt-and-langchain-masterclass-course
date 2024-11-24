9106570735a0eb7efb95d5b49814dbc4ed380b68
adriangmweb <adriangonzalezmesa@gmail.com>
2024-11-24 11:25:12 +0000

    using functions

--- agents/main.py
+++ agents/main.py
@@ -4,23 +4,30 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder
 )
+from langchain.schema import SystemMessage
 from langchain.agents import AgentExecutor, OpenAIFunctionsAgent
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 load_dotenv()
 
 
 chat = ChatOpenAI()
+
+tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=(
+            f"You are a helpful assistant that can answer questions about a SQLite database with the following tables: {tables}."
+            "Do not make any assumptions about what tables or columns exist. Instead, use the `describe_tables` tool to learn about the tables."
+        )),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat, 
--- agents/tools/sql.py
+++ agents/tools/sql.py
@@ -5,6 +5,19 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(f'{current_dir}/../db.sqlite')
 
+def list_tables() -> str:
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    return "\n".join([row[0] for row in rows if row[0] is not None])
+
+def describe_table(table_names: list[str]) -> str:
+    c = conn.cursor()
+    tables = ', '.join(f"'{table_name}'" for table_name in table_names)
+    rows = c.execute(f"SELECT sql FROM sqlite_master WHERE type='table' AND name IN ({tables});")
+    return "\n".join([row[0] for row in rows if row[0] is not None])
+
+
 def run_sqlite_query(query: str) -> str:
     c = conn.cursor()
     try:
@@ -17,4 +30,16 @@ run_query_tool = Tool.from_function(
     name="run_sqlite_query",
     description="Run a sqlite query against the database",
     func=run_sqlite_query
+)
+
+list_tables_tool = Tool.from_function(
+    name="list_tables",
+    description="List all tables in the database",
+    func=list_tables
+)
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of table names, describe the tables",
+    func=describe_table
 )
